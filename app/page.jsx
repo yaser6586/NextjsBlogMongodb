9c870df7b24c3fdadbb127227083e8f5528abb19
yaho <yaser.hoseini86@gmail.com>
2023-10-16 11:41:57 +0330

    move getpost function

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,8 @@
 import Post from "@/app/component/Post";
-import { API_URL } from "@/config/config";
 
-export async function getPost() {
-  const res = await fetch(`${API_URL}/api/posts`, {
-    cache: "no-store",
-  });
-  const PostData = res.json();
+import { getPost } from "@/lib/fetchData";
 
-  return PostData;
-}
+
 
 export default async function Home() {
   //for access to json json file we should use async await
